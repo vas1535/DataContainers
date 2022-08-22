9daf8bd5cc08047972441c770358648e60cf7c4e
user-2304 <user-2304@DESKTOP-H46TBLK>
2022-08-22 12:07:08 +0300

    Tree Day1

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -1,5 +1,25 @@
 //BinaryTree
 #include<iostream>
-using std::
-using std::
-using std::
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+#define delimeter "\n-------------------------------\n"
+
+class Tree
+{
+	class Element
+	{
+		int Data;
+		Element* pleft;
+		Element* pRight;
+	public:
+		Element(int Data, Element* pleft = nullptr, Element* pRigh)
+	};
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+}
